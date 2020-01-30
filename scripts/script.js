3d43c2de78b900640d32ebc9e6ae55cb27d1b5a4
GaunovMukhamed <gaunovmuhamed@gmail.com>
2020-01-30 17:36:17 +0300

    fixed todo

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,17 @@ const inputForm = document.querySelector(".add-todo-form");
 const ulNode = document.querySelector(".todo-list");
 const todoCross = document.querySelector(".todo-cross");
 
-inputForm.addEventListener("submit", addTodo);
+inputForm.addEventListener("submit", addTodo); ///adding
 
-ulNode.addEventListener("click", deleteTodo);
+ulNode.addEventListener("click", deleteTodo); ///deleting
+
+searchForm.addEventListener("keyup", filteringTodos); ///searching (filtering)
+
+function filteringTodos(event) {
+  const searchText = event.target.value;
+  const liArray = ulNode.children;
+  const excludedTodos = liArray.filter(item => !item.value.includes(searchText)).style.display = "none"; ///отредактировать
+}
 
 function deleteTodo(event) {
   if (event.target.className === "delete-button") {
