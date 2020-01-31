00249b2deb54db78460b9c81f81f978e2edc2ee7
GaunovMukhamed <gaunovmuhamed@gmail.com>
2020-01-31 17:42:38 +0300

    todo is almost done, fixed some issues

--- scripts/script.js
+++ scripts/script.js
@@ -2,39 +2,95 @@ const searchForm = document.querySelector(".search-todo-form");
 const inputForm = document.querySelector(".add-todo-form");
 const ulNode = document.querySelector(".todo-list");
 const todoCross = document.querySelector(".todo-cross");
+const bottomButtons = document.querySelector(".bottom-buttons");
+let checkboxesFlag = true;
 
 inputForm.addEventListener("submit", addTodo); ///adding
 
-ulNode.addEventListener("click", deleteTodo); ///deleting
+ulNode.addEventListener("click", interactTodo); ///deleting
 
-searchForm.addEventListener("keyup", filteringTodos); ///searching (filtering)
+searchForm.addEventListener("keyup", searchingTodos); ///searching (filtering)
 
-function filteringTodos(event) {
-  const searchText = event.target.value;
-  const liArray = ulNode.children;
-  const excludedTodos = liArray.filter(item => !item.value.includes(searchText)).style.display = "none"; ///отредактировать
+inputForm.addEventListener("click", toggleAllTodos); ///checking all todos
+
+bottomButtons.addEventListener("click", bottomButtonPress);
+
+function bottomButtonPress(event) {
+  const liArray = Array.from(ulNode.children);
+
+  if (event.target.className.includes("all-button")) {
+
+    liArray.forEach(item => item.classList.remove("hide-todo"));
+
+  } else if (event.target.className.includes("active-button")) { //without tick (check false)
+
+    liArray.forEach(item => item.querySelector(".checkbox").checked ? item.classList.add("hide-todo") : item.classList.remove("hide-todo"))
+
+  } else if (event.target.className.includes("completed-button")) { //with tick (check true)
+
+    liArray.forEach(item => item.querySelector(".checkbox").checked ? item.classList.remove("hide-todo") : item.classList.add("hide-todo"))
+  } else {
+    console.log('error');
+  }
+
 }
 
-function deleteTodo(event) {
-  if (event.target.className === "delete-button") {
-    event.target.parentElement.style.display = "none";
+function toggleAllTodos(event) {
+  if (event.target.className === "input-button") {
+    Array.from(ulNode.children).map(item => todoCheckToggle(item, checkboxesFlag));
+    checkboxesFlag = !checkboxesFlag;
   }
+}
+
+function searchingTodos(event) {
+  const searchText = event.target.value;
+  const liArray = Array.from(ulNode.children);
+  liArray.forEach(item => item.textContent.includes(searchText) ? item.classList.remove("hide-todo") : item.classList.add("hide-todo"));
+}
+
+function interactTodo(event) {
+  if (event.target.className === "delete-button") {
+    event.target.parentElement.classList.add("hide-todo");
+  } else {
+    todoCheckToggle(event.target.parentElement);;
+  }
+}
+
+function todoCheckToggle(liNode, allFlag) {
+  const checkbox = liNode.querySelector(".checkbox");
+  const todoText = liNode.querySelector(".todo-text");
+  if (!allFlag) {
+    checkbox.checked ? todoText.classList.remove("changed-todo-text") : todoText.classList.add("changed-todo-text");
+    checkbox.checked = !checkbox.checked;
+  } else {
+    if (allFlag) {
+      checkbox.checked = true;
+      todoText.classList.add("changed-todo-text");
+    } else {
+      checkbox.checked = false;
+      todoText.classList.remove("changed-todo-text");
+    }
+    console.log(checkbox.checked);
+  }
+
 }
 
 function addTodo(event) {
   event.preventDefault();
 
   const newTodoValue = inputForm.addInput.value;
+  inputForm.addInput.value = null;
 
   if (newTodoValue.length > 0) {
     todoRendering(newTodoValue);
   }
-
-
 }
 
 function todoRendering(newTodoValue) {
-  const htmlPattern = `<li class="new-todo"><input type="checkbox" class="todo-circle"><h2 class="todo-text">${newTodoValue}</h2><div class="delete-button"></div></li>`;
-
+  const htmlPattern = `<li class="new-todo">
+  <input type="checkbox" class="checkbox"><span class="custom-checkbox"></span>
+  <h2 class="todo-text">${newTodoValue}</h2>
+  <div class="delete-button"></div>
+</li>`;
   ulNode.innerHTML += htmlPattern;
 }
